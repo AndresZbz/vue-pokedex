abceb68c7e6214fc31901f88c3d246d690ae3c82
Juan Bouza <juanandresbouzab@gmail.com>
2024-06-02 23:01:49 -0400

    typo fixed

--- src/components/About.vue
+++ src/components/About.vue
@@ -8,7 +8,7 @@
         <div class="page-body text-white m-5">
             <h2>About</h2>
             <p>This is a Simple Pokédex made using Vue and bootstrap with the Vite tool.</p>
-            <p>All the API request was made using PokeAPI, you can check them in this <a class="text-info" href="https://pokeapi.co/">link</a> or their <a class="text-info" href="https://github.com/PokeAPI/pokeapi">Github</a></p>
+            <p>All the API request were made using PokeAPI, you can check them in this <a class="text-info" href="https://pokeapi.co/">link</a> or their <a class="text-info" href="https://github.com/PokeAPI/pokeapi">Github</a></p>
             <br>
             <h2>How to use</h2>
             <p>Go to <router-link to="/search" class="text-info">Search</router-link> or just click on the search navigation button and write the Pokémon name. (By default it searchs Pikachu info)</p>
